c3b73daa43ee442641b365c265f08495133c5990
Guillermo Montero Melis <montero-melis@biling.su.se>
2013-05-03 15:20:45 +0200

    adds arrange method to extract metainformation from parsed object

--- CHAarrange.py
+++ CHAarrange.py
@@ -1,4 +1,4 @@
-
+import re
 """Pseudocode for arranging the body of the transcription into right format"""
 
 ## transcription body:
@@ -32,46 +32,48 @@ def arrange(parsedObj):
 ## Metainformation
 def arrangeMeta(parsedObj):
 	meta = dict()
-
-	# Initialize variables for metainformation
-	# This is actually not needed, but put explanatory comments after each key-value assignment below
-	lang_interact = "" # language of interaction
-	ppt_name = "" # 'ppt' stands for participant
-	ppt_role = ""
-	ppt_gender = ""
-	ppt_group = ""
-	exp_name = "" # 'exp' stands for experimenter
-	transcr = "" # name of transcriber
-	order = ""
+	#These are the dictionary keys extracted from the transcription header:
+		# 'lang_interact': language of interaction
+		# 'ppt_name': participant name
+		# 'ppt_role': participant role
+		# 'ppt_gender': gender of participant
+		# 'ppt_group': group of the participant
+		# 'exp_name': experimenter name
+		# 'transcr': name of transcriber
+		# 'order': one of the four semi-randomized orders in which the videoclips were shown
 
 	for tup in parsedObj:
+		# Check language of interaction
 		if tup[0] == '@Languages':
 			meta['lang_interact'] = tup[1]
+		# Check participants' names and roles
 		elif tup[0] == '@Participants':
-			# Little regex needed here:
-			# split tup[1] by comma ','
-			# for each side of the comma:
-			# NB Write the conditional statements so that BOTH are checked
-				#if the 1st word == SUJ
-					# meta['ppt_name'] = 2nd word
-					# meta['ppt_role'] = 3rd word
-				# if 1st word == EXP
-					#meta['exp_name'] = 2nd word
+			listParticipants = tup[1].split(",")
+			for field in listParticipants:
+				if field.split()[0] == "SUJ":
+					meta['ppt_name'] = field.split()[1]	# participant name
+					meta['ppt_role'] = field.split()[2]	# participant role
+				elif field.split()[0] == "EXP":
+					meta['exp_name'] = field.split()[1]	#experimenter name
+		# Check other information about participants
 		elif tup[0] == '@ID':
-			listID = # split tup[1] by pipes '|' and create list whose elements are each field before a pipe
+			listID = tup[1].split("|")
+			# split tup[1] by pipes '|' and create list whose elements are each field before a pipe
 			if listID[2] == 'SUJ':
 				meta['ppt_gender'] = listID[4]
 				meta['ppt_group'] = listID[5]
 			elif listID[2] == 'EXP':
 				meta['exp_gender'] = listID[4]
+		# check transcriber's name
 		elif tup[0] == '@Transcriber':
 			meta['transcr'] = tup[1]
+		# check order in which the videos where shown (1--4)
 		elif tup[0] == '@Comment':
-			# This field is a bit more tricky because there might be different tipes of comments
-			# see if it matches with (case insensitive) 'ord'
-			# if true, grep whatever digit or group of digits come directly after that string (no whitespaces)
-			# the format might be '1' or '01', and there are only four orders 1,2,3,4 (or 01,02,03,04)
-				# meta['order'] = that number
+			r_order = re.compile(r"ord(er)? *0?([1-4])")
+			m = r_order.I(tup[1])
+			if m:
+				meta['order'] = m.group(2)
+	return meta
 
 
 
