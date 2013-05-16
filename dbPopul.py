4796dca6b1cf87d9df9eda46483f82cc7189af3c
Guillermo Montero Melis <montero-melis@biling.su.se>
2013-05-16 22:55:38 +0200

    Adds the script to populate the database

---
+++ dbPopul.py
@@ -0,0 +1,40 @@
+import sqlite3
+conn = sqlite3.connect('cha.db')
+c = conn.cursor()
+
+# 1. Schema
+
+c.execute('''CREATE TABLE IF NOT EXISTS language (id INTEGER PRIMARY KEY, language TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS experimenter (id INTEGER PRIMARY KEY, name TEXT, L1 INTEGER)''')
+
+# 2. Inserts SQL statements
+
+m = {   'exp_gender': 'male',
+    'exp_name': 'Guillermo',
+    'lang_interact': 'spa',
+    'order': '4',
+    'ppt_gender': 'female',
+    'ppt_group': 'SpAD',
+    'ppt_name': 'Spanish_Native_119',
+    'ppt_role': 'Target_Adult',
+    'transcr': 'guillermo'}
+
+c.execute('SELECT id FROM language WHERE language = ?', (m["lang_interact"],))
+row = c.fetchone()
+
+if row is not None:
+    id_lang_interact = row[0]
+else:
+    c.execute('INSERT INTO language VALUES (NULL, ?)', (m["lang_interact"],))
+    id_lang_interact = c.lastrowid    
+   
+c.execute('SELECT id FROM experimenter WHERE name = ?', (m["exp_name"],))
+row = c.fetchone()
+
+if row is not None:
+    id_experimenter = row[0]
+else:
+    c.execute('INSERT INTO experimenter VALUES (NULL, ?, ?)', (m["exp_name"], id_lang_interact))
+    id_experimenter = c.lastrowid
+   
+# and so forth...
