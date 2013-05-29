0b1f398fbbe8af141956bd8c19bbcc2c2d0cbb7e
Guillermo Montero Melis <montero-melis@biling.su.se>
2013-05-29 11:10:00 +0200

    Renames primary keys within each table simply to 'id' for consistency and ease of referencing; includes transcriber info into Interaction table

--- dbPopul.py
+++ dbPopul.py
@@ -21,65 +21,66 @@ def dbPopul(dbName, metainfo, body):
     # Variable names written with small caps
 
     # Tables created for normalization (look-up)
-    c.execute('''CREATE TABLE IF NOT EXISTS Language (lang_id INTEGER PRIMARY KEY, language TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS Gender (gend_id INTEGER PRIMARY KEY, gender TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS PartGroup (group_id INTEGER PRIMARY KEY, partGroup TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS Profile (prof_id INTEGER PRIMARY KEY, profile TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS Condition (cond_id INTEGER PRIMARY KEY, condition TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS Videos (video_id INTEGER PRIMARY KEY, videoname TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS Words (word_id INTEGER PRIMARY KEY, word TEXT, language INTEGER)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS Language (id INTEGER PRIMARY KEY, language TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS Gender (id INTEGER PRIMARY KEY, gender TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS PartGroup (id INTEGER PRIMARY KEY, partGroup TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS Profile (id INTEGER PRIMARY KEY, profile TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS Condition (id INTEGER PRIMARY KEY, condition TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS Videos (id INTEGER PRIMARY KEY, videoname TEXT, videotype TEXT)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS Words (id INTEGER PRIMARY KEY, word TEXT, language_id INTEGER)''')
 
 
     ## Tables with interesting relations
 
     # Experimenter
     c.execute('''CREATE TABLE IF NOT EXISTS Experimenter(
-        expe_id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         name TEXT,
-        gender INTEGER,
+        gender_id INTEGER,
         L1 INTEGER,
-        FOREIGN KEY(gender) REFERENCES Gender(gend_id),
-        FOREIGN KEY(L1) REFERENCES Language(lang_id)
+        FOREIGN KEY(gender_id) REFERENCES Gender(id),
+        FOREIGN KEY(L1) REFERENCES Language(id)
         )'''
     )
 
     # Participant
     c.execute('''CREATE TABLE IF NOT EXISTS Participant(
-        part_id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         name TEXT,
-        gender INTEGER,
+        gender_id INTEGER,
         L1 INTEGER,
-        partGroup INTEGER,
+        partGroup_id INTEGER,
         age REAL,
-        OQPTscore INTEGER,
-        FOREIGN KEY(gender) REFERENCES Gender(gend_id),
-        FOREIGN KEY(L1) REFERENCES Language(lang_id)
+        OQPTscore REAL,
+        FOREIGN KEY(gender_id) REFERENCES Gender(id),
+        FOREIGN KEY(L1) REFERENCES Language(id)
         )'''
     )
 
     # Interaction
     c.execute('''CREATE TABLE IF NOT EXISTS Interaction(
-        inte_id INTEGER PRIMARY KEY,
-        partName INTEGER,
-        expeName INTEGER,
-        langInteraction INTEGER,
+        id INTEGER PRIMARY KEY,
+        participant_id INTEGER,
+        experimenter_id INTEGER,
+        language_id INTEGER,
         orderLing INTEGER,
-        profile INTEGER,
-        condition INTEGER,
-        FOREIGN KEY(partName) REFERENCES Participant(part_id),    
-        FOREIGN KEY(expeName) REFERENCES Experimenter(expe_id),    
-        FOREIGN KEY(langInteraction) REFERENCES Language(lang_id),  
-        FOREIGN KEY(profile) REFERENCES Profile(prof_id),
-        FOREIGN KEY(condition) REFERENCES Condition(cond_id)
+        profile_id INTEGER,
+        condition_id INTEGER,
+        transcriber TEXT,
+        FOREIGN KEY(participant_id) REFERENCES Participant(id), 
+        FOREIGN KEY(experimenter_id) REFERENCES Experimenter(id),    
+        FOREIGN KEY(language_id) REFERENCES Language(id),  
+        FOREIGN KEY(profile_id) REFERENCES Profile(id),
+        FOREIGN KEY(condition_id) REFERENCES Condition(id)
         )'''
     )
 
     # DescrMatrix
     c.execute('''CREATE TABLE IF NOT EXISTS DescrMatrix(
-        interaction INTEGER,
-        video INTEGER,
+        interaction_id INTEGER,
+        video_id INTEGER,
         role TEXT,
-        word INTEGER
+        word_id INTEGER
         )'''
     )
 
@@ -92,12 +93,12 @@ def dbPopul(dbName, metainfo, body):
 
     ## Experimenter table and indexes -- add only a row to this table if new experimenter!
     # Check experimenter name
-    c.execute('SELECT expe_id FROM Experimenter WHERE name = ?', (m["exp_name"],))
+    c.execute('SELECT id FROM Experimenter WHERE name = ?', (m["exp_name"],))
     row = c.fetchone()
     if row is not None: # that is if experimenter already in db
         expe_name_id = row[0]
     else: # experimenter is not in the db
-        c.execute('SELECT gend_id FROM Gender WHERE gender = ?', (m["exp_gender"],)) # fetch gender
+        c.execute('SELECT id FROM Gender WHERE gender = ?', (m["exp_gender"],)) # fetch gender
         row = c.fetchone()
         if row is not None: # value for gender is already in db
             expe_gender_id = row[0]
@@ -110,7 +111,7 @@ def dbPopul(dbName, metainfo, body):
 
     ## Participant table and indexes
     # participant gender
-    c.execute('SELECT gend_id FROM Gender WHERE gender = ?', (m["ppt_gender"],))
+    c.execute('SELECT id FROM Gender WHERE gender = ?', (m["ppt_gender"],))
     row = c.fetchone()
     if row is not None:
         part_gender_id = row[0]
@@ -118,7 +119,7 @@ def dbPopul(dbName, metainfo, body):
         c.execute('INSERT INTO Gender VALUES (NULL,?)', (m["ppt_gender"],))
         part_gender_id = c.lastrowid
     # participant group
-    c.execute('SELECT group_id FROM PartGroup WHERE partGroup = ?', (m["ppt_group"],))
+    c.execute('SELECT id FROM PartGroup WHERE partGroup = ?', (m["ppt_group"],))
     row = c.fetchone()
     if row is not None:
         part_group_id = row[0]
@@ -135,7 +136,7 @@ def dbPopul(dbName, metainfo, body):
 
     ## Interaction table
     # language of interaction
-    c.execute('SELECT lang_id FROM Language WHERE language = ?', (m["lang_interact"],))
+    c.execute('SELECT id FROM Language WHERE language = ?', (m["lang_interact"],))
     row = c.fetchone()
     if row is not None:
         interaction_lang_id = row[0]
@@ -144,26 +145,28 @@ def dbPopul(dbName, metainfo, body):
         interaction_lang_id = c.lastrowid    
     # orderLing (order in which linguistic task is performed)
     orderLing = m["order"]
+    # Transcriber
+    transcr = m["transcr"]
     # Populate Interaction table
-    c.execute('INSERT INTO Interaction VALUES (NULL,?,?,?,?,NULL,NULL)', (part_name_id, expe_name_id, interaction_lang_id, orderLing))
+    c.execute('INSERT INTO Interaction VALUES (NULL,?,?,?,?,NULL,NULL,?)', (part_name_id, expe_name_id, interaction_lang_id, orderLing, transcr))
     interaction_id = c.lastrowid
 
     ## DescrMatrix table
     for turn in t: # Each turn refers to what each participant (subject, experimenter) says for each description
         # Parse video (turn[0]), enter it to Videos if necessary, and keep its id as a variable video_id
-        c.execute('SELECT video_id FROM Videos WHERE videoname = ?', (turn[0],))
+        c.execute('SELECT id FROM Videos WHERE videoname = ?', (turn[0],))
         row = c.fetchone()
         if row is not None:
             video_id = row[0]
         else:
-            c.execute('INSERT INTO Videos VALUES (NULL, ?)', (turn[0],))
+            c.execute('INSERT INTO Videos VALUES (NULL, ?, NULL)', (turn[0],))
             video_id = c.lastrowid    
         # Update variable 'role' with value turn[1]
         role = turn[1]
         # For each word in turn[2] (use split method!):
         for eachword in turn[2].split():
             # Parse each word, enter into Words if necessary, keep it id as variable word_id
-            c.execute('SELECT word_id FROM Words WHERE word = ? AND language = ?', (unicode(eachword, 'utf-8'), interaction_lang_id))
+            c.execute('SELECT id FROM Words WHERE word = ? AND language_id = ?', (unicode(eachword, 'utf-8'), interaction_lang_id))
             row = c.fetchone()
             if row is not None:
                 word_id = row[0]
