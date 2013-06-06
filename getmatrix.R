4af341a9656427467153c6c6a7f8f9ccd9ffe3b5
Guillermo Montero Melis <montero-melis@biling.su.se>
2013-06-06 22:25:39 +0200

    Minor changes to getmatrix

--- getmatrix.R
+++ getmatrix.R
@@ -3,6 +3,7 @@
 
 library("RSQLite")
 library("reshape")
+library("lsa")
 
 m <- dbDriver("SQLite")
 con <- dbConnect(m, dbname = "cha.db")
@@ -16,6 +17,14 @@ sample <- dbGetQuery(con, "-- Data to convert into matrix
   	INNER JOIN Interaction i ON dm.interaction_id=i.id
   	INNER JOIN Participant p ON i.participant_id=p.id
   WHERE dm.role='*SUJ'
-  LIMIT 50
+  LIMIT 200
 ")
 
+book.sales = read.csv("http://news.mrdwab.com/data-booksales")
+
+sample$name <- as.factor(sample$name)
+sample$videoname <- as.factor(sample$videoname)
+sample$word <- as.factor(sample$word)
+
+m.sample <- melt(sample, id.vars=1:3)
+ cast(sample, ... ~ videoname)
